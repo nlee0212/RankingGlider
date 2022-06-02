42728cc1024b805a371fcf1268193c1dcb62935f
nlee0212 <nlee0212@kaist.ac.kr>
2022-06-03 00:55:45 +0900

    create compile file

---
+++ compile_ranking_glider.sh
@@ -0,0 +1,6 @@
+g++ -Wall -o rg-config1 RGlider/ranking_glider.cc lib/config1.a
+g++ -Wall -o rg-config2 RGlider/ranking_glider.cc lib/config2.a
+# g++ -Wall --std=c++11 -o lru-config3 example/lru-8MB.cc lib/config3.a
+# g++ -Wall --std=c++11 -o lru-config4 example/lru-8MB.cc lib/config4.a
+# g++ -Wall --std=c++11 -o lru-config5 example/lru-8MB.cc lib/config5.a
+# g++ -Wall --std=c++11 -o lru-config6 example/lru-8MB.cc lib/config6.a
