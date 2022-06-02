c1d7d357682151111ea8447db80df4c6173c7e2f
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-02 16:34:30 +0000

    add run helper files

---
+++ run_hawkeye.sh
@@ -0,0 +1,3 @@
+./hawkeye-config1 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./hawkeye-config2 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+
---
+++ run_ranking_glider.sh
@@ -0,0 +1,2 @@
+./rg-config1 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
+./rg-config2 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
