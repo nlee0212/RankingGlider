234f05592e0e8f3c20df845d017aad53bb21dc19
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-06 13:56:45 +0000

    set hawkeye compilers

--- compile_hawkeye.sh
+++ compile_hawkeye.sh
@@ -1,6 +1,6 @@
 g++ -Wall -o hawkeye-config1 Hawkeye/hawkeye_final.cc lib/config1.a
 g++ -Wall -o hawkeye-config2 Hawkeye/hawkeye_final.cc lib/config2.a
-# g++ -Wall --std=c++11 -o lru-config3 example/lru-8MB.cc lib/config3.a
-# g++ -Wall --std=c++11 -o lru-config4 example/lru-8MB.cc lib/config4.a
-# g++ -Wall --std=c++11 -o lru-config5 example/lru-8MB.cc lib/config5.a
-# g++ -Wall --std=c++11 -o lru-config6 example/lru-8MB.cc lib/config6.a
+g++ -Wall -o hawkeye-config3 Hawkeye/hawkeye_final_8MB.cc lib/config3.a
+g++ -Wall -o hawkeye-config4 Hawkeye/hawkeye_final_8MB.cc lib/config4.a
+g++ -Wall -o hawkeye-config5 Hawkeye/hawkeye_final_8MB.cc lib/config5.a
+g++ -Wall -o hawkeye-config6 Hawkeye/hawkeye_final_8MB.cc lib/config6.a
