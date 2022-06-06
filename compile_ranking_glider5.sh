ade5a749019e30bc8ca3076c49ad363004bbd197
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-06 21:02:17 +0000

    build runners for rg5

---
+++ compile_ranking_glider5.sh
@@ -0,0 +1,6 @@
+g++ -Wall -o rg5-config1 RGlider/ranking_glider5.cc lib/config1.a
+g++ -Wall -o rg5-config2 RGlider/ranking_glider5.cc lib/config2.a
+g++ -Wall -o rg5-config3 RGlider/ranking_glider_8MB_5.cc lib/config3.a
+g++ -Wall -o rg5-config4 RGlider/ranking_glider_8MB_5.cc lib/config4.a
+g++ -Wall -o rg5-config5 RGlider/ranking_glider_8MB_5.cc lib/config5.a
+g++ -Wall -o rg5-config6 RGlider/ranking_glider_8MB_5.cc lib/config6.a
