f70f8be22c90c45117518555ca09182bf05a1514
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-06 07:10:25 +0000

    add compilers for 8MB ver.

--- compile_ranking_glider.sh
+++ compile_ranking_glider.sh
@@ -1,6 +1,6 @@
 g++ -Wall -o rg-config1 RGlider/ranking_glider.cc lib/config1.a
 g++ -Wall -o rg-config2 RGlider/ranking_glider.cc lib/config2.a
-# g++ -Wall --std=c++11 -o lru-config3 example/lru-8MB.cc lib/config3.a
-# g++ -Wall --std=c++11 -o lru-config4 example/lru-8MB.cc lib/config4.a
-# g++ -Wall --std=c++11 -o lru-config5 example/lru-8MB.cc lib/config5.a
-# g++ -Wall --std=c++11 -o lru-config6 example/lru-8MB.cc lib/config6.a
+g++ -Wall -o rg-config3 RGlider/ranking_glider_8MB.cc lib/config3.a
+g++ -Wall -o rg-config4 RGlider/ranking_glider_8MB.cc lib/config4.a
+g++ -Wall -o rg-config5 RGlider/ranking_glider_8MB.cc lib/config5.a
+g++ -Wall -o rg-config6 RGlider/ranking_glider_8MB.cc lib/config6.a
