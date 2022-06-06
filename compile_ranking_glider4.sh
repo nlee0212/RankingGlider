d71366987fc37ea7976388ca31bb5de314607bed
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-06 21:12:56 +0000

    lru for cache pc hist

---
+++ compile_ranking_glider4.sh
@@ -0,0 +1,6 @@
+g++ -Wall -o rg4-config1 RGlider/ranking_glider4.cc lib/config1.a
+g++ -Wall -o rg4-config2 RGlider/ranking_glider4.cc lib/config2.a
+g++ -Wall -o rg4-config3 RGlider/ranking_glider_8MB_4.cc lib/config3.a
+g++ -Wall -o rg4-config4 RGlider/ranking_glider_8MB_4.cc lib/config4.a
+g++ -Wall -o rg4-config5 RGlider/ranking_glider_8MB_4.cc lib/config5.a
+g++ -Wall -o rg4-config6 RGlider/ranking_glider_8MB_4.cc lib/config6.a
