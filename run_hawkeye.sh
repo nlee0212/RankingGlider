c5729a23101ef4262c858929715a93680048201c
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-06 13:57:28 +0000

    set hawkeye runners

--- run_hawkeye.sh
+++ run_hawkeye.sh
@@ -1,3 +1,6 @@
-./hawkeye-config1 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
-./hawkeye-config2 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz
-
+./hawkeye-config1 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz | tee -a hawkeye_output.txt
+./hawkeye-config2 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz | tee -a hawkeye_output.txt
+./hawkeye-config3 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz trace/mcf_10M.trace.gz trace/libquantum_10M.trace.gz trace/xalancbmk_10M.trace.gz | tee -a hawkeye_output.txt
+./hawkeye-config4 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz trace/mcf_10M.trace.gz trace/libquantum_10M.trace.gz trace/xalancbmk_10M.trace.gz | tee -a hawkeye_output.txt
+./hawkeye-config3 -warmup_instructions 1000000 -simulation_instructions 10000000 -cloudsuite -traces trace/cassandra_core_0.trace.gz trace/cassandra_core_1.trace.gz trace/cassandra_core_2.trace.gz trace/cassandra_core_3.trace.gz | tee -a hawkeye_output.txt
+./hawkeye-config4 -warmup_instructions 1000000 -simulation_instructions 10000000 -cloudsuite -traces trace/cassandra_core_0.trace.gz trace/cassandra_core_1.trace.gz trace/cassandra_core_2.trace.gz trace/cassandra_core_3.trace.gz | tee -a hawkeye_output.txt
