ef76c08f6154af1a962e2ac8efe2fb1a20813d20
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-06 18:33:29 +0000

    add runners for rg3

---
+++ compile_ranking_glider3.sh
@@ -0,0 +1,6 @@
+g++ -Wall -o rg3-config1 RGlider/ranking_glider3.cc lib/config1.a
+g++ -Wall -o rg3-config2 RGlider/ranking_glider3.cc lib/config2.a
+g++ -Wall -o rg3-config3 RGlider/ranking_glider_8MB_3.cc lib/config3.a
+g++ -Wall -o rg3-config4 RGlider/ranking_glider_8MB_3.cc lib/config4.a
+g++ -Wall -o rg3-config5 RGlider/ranking_glider_8MB_3.cc lib/config5.a
+g++ -Wall -o rg3-config6 RGlider/ranking_glider_8MB_3.cc lib/config6.a
---
+++ run_ranking_glider3.sh
@@ -0,0 +1,6 @@
+./rg3-config1 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz | tee -a rg3_output.txt
+./rg3-config2 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz | tee -a rg3_output.txt
+./rg3-config3 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz trace/mcf_10M.trace.gz trace/libquantum_10M.trace.gz trace/xalancbmk_10M.trace.gz | tee -a rg3_output.txt
+./rg3-config4 -warmup_instructions 1000000 -simulation_instructions 10000000 -traces trace/bzip2_10M.trace.gz trace/mcf_10M.trace.gz trace/libquantum_10M.trace.gz trace/xalancbmk_10M.trace.gz | tee -a rg3_output.txt
+./rg3-config3 -warmup_instructions 1000000 -simulation_instructions 10000000 -cloudsuite -traces trace/cassandra_core_0.trace.gz trace/cassandra_core_1.trace.gz trace/cassandra_core_2.trace.gz trace/cassandra_core_3.trace.gz | tee -a rg3_output.txt
+./rg3-config4 -warmup_instructions 1000000 -simulation_instructions 10000000 -cloudsuite -traces trace/cassandra_core_0.trace.gz trace/cassandra_core_1.trace.gz trace/cassandra_core_2.trace.gz trace/cassandra_core_3.trace.gz | tee -a rg3_output.txt
