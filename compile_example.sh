deaa0c21552e836bee9df21ba15e227cc1d94e04
Na Yeon Lee <nlee0212@kaist.ac.kr>
2022-06-08 11:21:13 +0000

    add srrip compilers

--- compile_example.sh
+++ compile_example.sh
@@ -1,6 +1,12 @@
-g++ -Wall --std=c++11 -o lru-config1 example/lru.cc lib/config1.a
-g++ -Wall --std=c++11 -o lru-config2 example/lru.cc lib/config2.a
-g++ -Wall --std=c++11 -o lru-config3 example/lru-8MB.cc lib/config3.a
-g++ -Wall --std=c++11 -o lru-config4 example/lru-8MB.cc lib/config4.a
-g++ -Wall --std=c++11 -o lru-config5 example/lru-8MB.cc lib/config5.a
-g++ -Wall --std=c++11 -o lru-config6 example/lru-8MB.cc lib/config6.a
+g++ -Wall -o lru-config1 example/lru.cc lib/config1.a
+g++ -Wall -o lru-config2 example/lru.cc lib/config2.a
+g++ -Wall -o lru-config3 example/lru-8MB.cc lib/config3.a
+g++ -Wall -o lru-config4 example/lru-8MB.cc lib/config4.a
+g++ -Wall -o lru-config5 example/lru-8MB.cc lib/config5.a
+g++ -Wall -o lru-config6 example/lru-8MB.cc lib/config6.a
+g++ -Wall -o srrip-config1 example/srrip.cc lib/config1.a
+g++ -Wall -o srrip-config2 example/srrip.cc lib/config2.a
+g++ -Wall -o srrip-config3 example/srrip-8MB.cc lib/config3.a
+g++ -Wall -o srrip-config4 example/srrip-8MB.cc lib/config4.a
+g++ -Wall -o srrip-config5 example/srrip-8MB.cc lib/config5.a
+g++ -Wall -o srrip-config6 example/srrip-8MB.cc lib/config6.a
